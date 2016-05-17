ae79f7ef0c08d796f26207596a8bb1113b2a7b13
Jess Jacobs <akisma@gmail.com>
2016-05-17 01:14:22 -0400

    content and styles, barebones

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 //server
 
 var koa = require('koa');
+var fs = require('fs');
+
+var readFileThunk = function(src) {
+  return new Promise(function (resolve, reject) {
+    fs.readFile(src, {'encoding': 'utf8'}, function (err, data) {
+      if(err) return reject(err);
+      resolve(data);
+    });
+  });
+}
 
 var app = koa();
 
-app.use(function *(){
-  this.body = 'Hello World';
-});
+app.use(router.get('/', function *(){
+  this.body = yield readFileThunk(__dirname + '/index.html');
+}));
 
-app.listen(80);
+app.listen(8080);
 console.log('Koa listening on port 80');
